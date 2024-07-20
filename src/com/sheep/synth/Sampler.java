968af4449261efbf89c0aee3cd8497ef83326fe6
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2024-07-20 14:58:41 -0500

    add drafty volume control on each voice

--- src/com/sheep/synth/Sampler.java
+++ src/com/sheep/synth/Sampler.java
@@ -10,10 +10,12 @@ import java.awt.image.BufferedImage;
 
 public class Sampler extends SynthControlContainer implements AudioSource {
     private static final int TONE_OFFSET_LIMIT = 2000;
+    private static final int LEVEL_MULTIPLIER_LIMIT = 1000;
 
     private double baseFrequency;
     private double frequency; // true frequency once toneOffset is applied
     private int toneOffset; // logarithmic-scale pitch offset. 1000 is one octave
+    private int levelMultiplier = 1000;
     private AudioTrack wavetable;
     private double wavetablePos;
     private double wavetableStepSize;
@@ -21,7 +23,7 @@ public class Sampler extends SynthControlContainer implements AudioSource {
     public Sampler(Synthesizer synth) {
         super(synth);
 
-        wavetable = new AudioTrack("sounds/meow_trimmed.ogg");
+        wavetable = new AudioTrack("sounds/scratch_meow_trimmed.ogg");
         // JComboBox<Waveform> comboBox = new JComboBox<>(Waveform.values());
         // comboBox.setSelectedItem(Waveform.Sine);
         // comboBox.setBounds(10, 10, 75, 25);
@@ -83,6 +85,57 @@ public class Sampler extends SynthControlContainer implements AudioSource {
         toneText.setBounds(160, 40, 75, 25);
         add(toneText);
 
+
+        JLabel levelParameter = new JLabel(" 1.000");
+        levelParameter.setBounds(220, 65, 50, 25);
+        levelParameter.setBorder(Utils.WindowDesign.LINE_BORDER);
+        levelParameter.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                final Cursor BLANK_CURSOR = Toolkit.getDefaultToolkit()
+                .createCustomCursor(new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB), new Point(0, 0), "blank_cursor");
+                setCursor(BLANK_CURSOR);
+                mouseClickLocation = e.getLocationOnScreen();
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setCursor(Cursor.getDefaultCursor());
+            }
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    if (levelMultiplier == 1000) {
+                        levelMultiplier = 0;
+                    } else {
+                        levelMultiplier = 1000;
+                    }
+                    levelParameter.setText(String.format(" %.3f", getLevelMultiplier()));
+                }
+            }
+        });
+        levelParameter.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) { // TODO: experiment with different functions mapping dy to toneOffset increment
+                int dy = e.getYOnScreen() - mouseClickLocation.y; // note: pos y is downward
+                if (dy != 0) {
+                    if (dy < 0 && levelMultiplier < LEVEL_MULTIPLIER_LIMIT) {
+                        levelMultiplier++;
+                    } else if (dy > 0 && levelMultiplier > 0) {
+                        levelMultiplier--;
+                    }
+                    levelParameter.setText(String.format(" %.3f", getLevelMultiplier()));
+                    // applyToneOffset();
+
+                }
+                Utils.ParameterHandling.PARAMETER_ROBOT.mouseMove(mouseClickLocation.x, mouseClickLocation.y);
+            }
+        });
+        add(levelParameter);
+
+        JLabel levelText = new JLabel("Level");
+        levelText.setBounds(215, 40, 75, 25);
+        add(levelText);
+
         setSize(275, 100);
         setBorder(Utils.WindowDesign.LINE_BORDER);
         setLayout(null);
@@ -90,6 +143,10 @@ public class Sampler extends SynthControlContainer implements AudioSource {
 
     public double getBaseFrequency() {
         return baseFrequency;
+    }
+
+    public double getLevelMultiplier() {
+        return levelMultiplier / 1000d;
     }
 
     public void setBaseFrequency(double baseFrequency) {
@@ -103,10 +160,8 @@ public class Sampler extends SynthControlContainer implements AudioSource {
     
     private void applyToneOffset() {
         frequency = baseFrequency * Math.pow(2, getToneOffset());
-        // wavetablePos = 0; // seek to start. feels natural to do this
-        // double multiplier = Math.pow(2, getToneOffset());
 
-        wavetableStepSize = 3*frequency / 626 * wavetable.sampleRate / Synthesizer.AudioInfo.SAMPLE_RATE; // the magic number is to tune the sample
+        wavetableStepSize = 4*frequency / 626 * wavetable.sampleRate / Synthesizer.AudioInfo.SAMPLE_RATE; // the magic number is to tune the sample
     }
     
     public double nextSample() {
@@ -124,7 +179,8 @@ public class Sampler extends SynthControlContainer implements AudioSource {
         short samp_l = wavetable.fetchSample(left);
         short samp_r = wavetable.fetchSample(right);
 
-        return (double) (samp_l + frac * (samp_r - samp_l)) / Short.MAX_VALUE;
+        double sample = (double) (samp_l + frac * (samp_r - samp_l)) / Short.MAX_VALUE;
+        return sample * levelMultiplier / 1000d;
     }
 
     public void seekStart() {
